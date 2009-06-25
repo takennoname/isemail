c4d795b9795b619ac0dafba8ec0f74849dd2f334
dominic.sayers <dominic.sayers@gmail.com>
2009-06-25 17:33:42 +0000

    Version 1.11 - License terms changed to BSD

--- is_email.php
+++ is_email.php
@@ -3,10 +3,39 @@
  * @package	isemail
  * @author	Dominic Sayers <dominic_sayers@hotmail.com>
  * @copyright	2009 Dominic Sayers
- * @license	http://www.opensource.org/licenses/cpal_1.0 Common Public Attribution License Version 1.0 (CPAL) license
+ * @license	http://www.opensource.org/licenses/bsd-license.php BSD License
  * @link	http://www.dominicsayers.com/isemail
- * @version	1.9 - Minor modifications to make it compatible with PHPLint
+ * @version	1.11 - License terms changed to BSD
  */
+
+/*
+Copyright (c) 2008-2009, Dominic Sayers
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of Dominic Sayers nor the names of its contributors may be
+   used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 /*.
 	require_module 'standard';
 	require_module 'pcre';
@@ -125,7 +154,7 @@
 		// Remove any leading or trailing FWS
 		$element = preg_replace("/^$FWS|$FWS\$/", '', $element);
 
-		// Then we need to remove all valid comments (i.e. those at the start or end of the element
+		// Then we need to remove all valid comments (i.e. those at the start or end of the element)
 		$elementLength = strlen($element);
 
 		if ($element[0] === '(') {
