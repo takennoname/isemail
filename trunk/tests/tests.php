c05e4ff5afd852be38bdfe813200c7448b93ef8d
dominic.sayers <dominic.sayers@416575fc-f6c0-11dd-b507-8b4ecd5210be>
2010-01-16 22:31:16 +0000

    Version 1.16 - Added optional diagnosis codes for invalid addresses

--- trunk/tests/tests.php
+++ trunk/tests/tests.php
@@ -8,7 +8,7 @@
 <style type="text/css">
 div {clear:left;}
 p {font-family:Segoe UI,Arial,Helvetica,sans-serif;font-size:12px;margin:0;padding:0;float:left;}
-p.valid {width:60px;}
+p.valid {width:90px;}
 p.address {text-align:right;width:400px;overflow:hidden;margin-right:8px;}
 p.author {font-style:italic;}
 hr {clear:left;}
@@ -20,16 +20,17 @@ hr {clear:left;}
 require_once '../is_email.php';
 
 function unitTest ($email, $expected, $comment = '') {
-	$valid		= is_email($email);
-	$not		= ($valid) ? 'Valid' : 'Not valid';
+	$diagnosis	= is_email($email, false, true);
+	$valid		= ($diagnosis === ISEMAIL_VALID);
+	$not		= ($valid) ? 'Valid' : "Not valid ($diagnosis)";
 	$unexpected	= ($valid !== $expected) ? " <b>$not</b>" : "$not";
-	$comment		= ($comment === '') ? "&nbsp;" : stripslashes("$comment");
+	$comment	= ($comment === '') ? "&nbsp;" : stripslashes("$comment");
 	
 	return "<div><p class=\"address\"<em>$email</em></p><p class=\"valid\">$unexpected</p><p class=\"comment\">$comment</p></div>
 ";
 }
 
-echo "<h3>Email address validation test suite version 1.8</h3>\n";
+echo "<h3>Email address validation test suite version 1.9</h3>\n";
 echo "<p class=\"author\">Dominic Sayers | <a href=\"mailto:dominic_sayers@hotmail.com\">dominic_sayers@hotmail.com</a> | <a href=\"http://www.dominicsayers.com/isemail\">RFC-compliant email address validation</a></p>\n<br>\n<hr>\n";
 echo unitTest("first.last@example.com", true, "");
 echo unitTest("1234567890123456789012345678901234567890123456789012345678901234@example.com", true, "");
@@ -65,8 +66,8 @@ echo unitTest("\"\"@example.com", false, "Local part is effectively empty");
 echo unitTest("first\\\\@last@example.com", false, "Local part contains unescaped excluded characters");
 echo unitTest("first.last@", false, "No domain");
 echo unitTest("x@x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456789.x23456", false, "Domain exceeds 255 chars");
-echo unitTest("first.last@[.12.34.56.78]", false, "Only char that can precede IPv4 address is \':\'");
-echo unitTest("first.last@[12.34.56.789]", false, "Can\'t be interpreted as IPv4 so IPv6 tag is missing");
+echo unitTest("first.last@[.12.34.56.78]", false, "Only char that can precede IPv4 address is ':'");
+echo unitTest("first.last@[12.34.56.789]", false, "Can't be interpreted as IPv4 so IPv6 tag is missing");
 echo unitTest("first.last@[::12.34.56.78]", false, "IPv6 tag is missing");
 echo unitTest("first.last@[IPv5:::12.34.56.78]", false, "IPv6 tag is wrong");
 echo unitTest("first.last@[IPv6:1111:2222:3333::4444:5555:12.34.56.78]", false, "Too many IPv6 groups (4 max)");
@@ -74,15 +75,15 @@ echo unitTest("first.last@[IPv6:1111:2222:3333:4444:5555:12.34.56.78]", false, "
 echo unitTest("first.last@[IPv6:1111:2222:3333:4444:5555:6666:7777:12.34.56.78]", false, "Too many IPv6 groups (6 max)");
 echo unitTest("first.last@[IPv6:1111:2222:3333:4444:5555:6666:7777]", false, "Not enough IPv6 groups");
 echo unitTest("first.last@[IPv6:1111:2222:3333:4444:5555:6666:7777:8888:9999]", false, "Too many IPv6 groups (8 max)");
-echo unitTest("first.last@[IPv6:1111:2222::3333::4444:5555:6666]", false, "Too many \'::\' (can be none or one)");
+echo unitTest("first.last@[IPv6:1111:2222::3333::4444:5555:6666]", false, "Too many '::' (can be none or one)");
 echo unitTest("first.last@[IPv6:1111:2222:3333::4444:5555:6666:7777]", false, "Too many IPv6 groups (6 max)");
 echo unitTest("first.last@[IPv6:1111:2222:333x::4444:5555]", false, "x is not valid in an IPv6 address");
 echo unitTest("first.last@[IPv6:1111:2222:33333::4444:5555]", false, "33333 is not a valid group in an IPv6 address");
-echo unitTest("first.last@example.123", false, "TLD can\'t be all digits");
+echo unitTest("first.last@example.123", false, "TLD can't be all digits");
 echo unitTest("first.last@com", false, "Mail host must be second- or lower level");
-echo unitTest("first.last@-xample.com", false, "Label can\'t begin with a hyphen");
-echo unitTest("first.last@exampl-.com", false, "Label can\'t end with a hyphen");
-echo unitTest("first.last@x234567890123456789012345678901234567890123456789012345678901234.example.com", false, "Label can\'t be longer than 63 octets");
+echo unitTest("first.last@-xample.com", false, "Label can't begin with a hyphen");
+echo unitTest("first.last@exampl-.com", false, "Label can't end with a hyphen");
+echo unitTest("first.last@x234567890123456789012345678901234567890123456789012345678901234.example.com", false, "Label can't be longer than 63 octets");
 echo unitTest("\"Abc\\@def\"@example.com", true, "");
 echo unitTest("\"Fred\\ Bloggs\"@example.com", true, "");
 echo unitTest("\"Joe.\\\\Blow\"@example.com", true, "");
@@ -127,7 +128,7 @@ echo unitTest("\"test.test\"@example.com", true, "");
 echo unitTest("test.\"test\"@example.com", true, "Obsolete form, but documented in RFC2822");
 echo unitTest("\"test@test\"@example.com", true, "");
 echo unitTest("test@123.123.123.x123", true, "");
-echo unitTest("test@123.123.123.123", false, "Top Level Domain won\'t be all-numeric (see RFC3696 Section 2). I disagree with Dave Child on this one.");
+echo unitTest("test@123.123.123.123", false, "Top Level Domain won't be all-numeric (see RFC3696 Section 2). I disagree with Dave Child on this one.");
 echo unitTest("test@[123.123.123.123]", true, "");
 echo unitTest("test@example.example.com", true, "");
 echo unitTest("test@example.example.example.com", true, "");
@@ -189,12 +190,12 @@ echo unitTest("Abc\\@def@example.com", false, "Was incorrectly given as a valid 
 echo unitTest("Fred\\ Bloggs@example.com", false, "Was incorrectly given as a valid address in the original RFC3696");
 echo unitTest("Joe.\\\\Blow@example.com", false, "Was incorrectly given as a valid address in the original RFC3696");
 echo unitTest("first.last@[IPv6:1111:2222:3333:4444:5555:6666:12.34.567.89]", false, "IPv4 part contains an invalid octet");
-echo unitTest("\"test\\\r\n blah\"@example.com", false, "Folding white space can\'t appear within a quoted pair");
+echo unitTest("\"test\\\r\n blah\"@example.com", false, "Folding white space can't appear within a quoted pair");
 echo unitTest("\"test\r\n blah\"@example.com", true, "This is a valid quoted string with folding white space");
-echo unitTest("{^c\\@**Dog^}@cartoon.com", false, "This is a throwaway example from Doug Lovell\'s article. Actually it\'s not a valid address.");
+echo unitTest("{^c\\@**Dog^}@cartoon.com", false, "This is a throwaway example from Doug Lovell's article. Actually it's not a valid address.");
 echo unitTest("(foo)cal(bar)@(baz)iamcal.com(quux)", true, "A valid address containing comments");
 echo unitTest("cal@iamcal(woo).(yay)com", true, "A valid address containing comments");
-echo unitTest("\"foo\"(yay)@(hoopla)[1.2.3.4]", false, "Address literal can\'t be commented (RFC5321)");
+echo unitTest("\"foo\"(yay)@(hoopla)[1.2.3.4]", false, "Address literal can't be commented (RFC5321)");
 echo unitTest("cal(woo(yay)hoopla)@iamcal.com", true, "A valid address containing comments");
 echo unitTest("cal(foo\\@bar)@iamcal.com", true, "A valid address containing comments");
 echo unitTest("cal(foo\\)bar)@iamcal.com", true, "A valid address containing comments and an escaped parenthesis");
@@ -206,14 +207,14 @@ echo unitTest("first.(\r\n middle\r\n )last@example.com", true, "Comment with fo
 echo unitTest("first(12345678901234567890123456789012345678901234567890)last@(1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890)example.com", false, "Too long with comments, not too long without");
 echo unitTest("first(Welcome to\r\n the (\"wonderful\" (!)) world\r\n of email)@example.com", true, "Silly example from my blog post");
 echo unitTest("pete(his account)@silly.test(his host)", true, "Canonical example from RFC5322");
-echo unitTest("c@(Chris\'s host.)public.example", true, "Canonical example from RFC5322");
+echo unitTest("c@(Chris's host.)public.example", true, "Canonical example from RFC5322");
 echo unitTest("jdoe@machine(comment).  example", true, "Canonical example from RFC5322");
 echo unitTest("1234   @   local(blah)  .machine .example", true, "Canonical example from RFC5322");
-echo unitTest("first(middle)last@example.com", false, "Can\'t have a comment or white space except at an element boundary");
+echo unitTest("first(middle)last@example.com", false, "Can't have a comment or white space except at an element boundary");
 echo unitTest("first(abc.def).last@example.com", true, "Comment can contain a dot");
 echo unitTest("first(a\"bc.def).last@example.com", true, "Comment can contain double quote");
 echo unitTest("first.(\")middle.last(\")@example.com", true, "Comment can contain a quote");
-echo unitTest("first(abc(\"def\".ghi).mno)middle(abc(\"def\".ghi).mno).last@(abc(\"def\".ghi).mno)example(abc(\"def\".ghi).mno).(abc(\"def\".ghi).mno)com(abc(\"def\".ghi).mno)", false, "Can\'t have comments or white space except at an element boundary");
+echo unitTest("first(abc(\"def\".ghi).mno)middle(abc(\"def\".ghi).mno).last@(abc(\"def\".ghi).mno)example(abc(\"def\".ghi).mno).(abc(\"def\".ghi).mno)com(abc(\"def\".ghi).mno)", false, "Can't have comments or white space except at an element boundary");
 echo unitTest("first(abc\\(def)@example.com", true, "Comment can contain quoted-pair");
 echo unitTest("first.last@x(1234567890123456789012345678901234567890123456789012345678901234567890).com", true, "Label is longer than 63 octets, but not with comment removed");
 echo unitTest("a(a(b(c)d(e(f))g)h(i)j)@example.com", true, "");
@@ -248,7 +249,7 @@ echo unitTest("shaitan@my-domain.thisisminekthx", true, "Disagree with Paul Greg
 echo unitTest("test@...........com", false, "......");
 echo unitTest("foobar@192.168.0.1", false, "ip need to be []");
 echo unitTest("\"Joe\\\\Blow\"@example.com", true, "");
-echo unitTest("Invalid \\\n Folding \\\n Whitespace@example.com", false, "This isn\'t FWS so Dominic Sayers says it\'s invalid");
+echo unitTest("Invalid \\\n Folding \\\n Whitespace@example.com", false, "This isn't FWS so Dominic Sayers says it's invalid");
 echo unitTest("HM2Kinsists@(that comments are allowed)this.is.ok", true, "");
 echo unitTest("user%uucp!path@somehost.edu", true, "");
 echo unitTest("\"first(last)\"@example.com", true, "");
@@ -256,9 +257,10 @@ echo unitTest(" \r\n (\r\n x \r\n ) \r\n first\r\n ( \r\n x\r\n ) \r\n .\r\n ( \
 echo unitTest("test. \r\n \r\n obs@syntax.com", true, "obs-fws allows multiple lines");
 echo unitTest("test. \r\n \r\n obs@syntax.com", true, "obs-fws allows multiple lines (test 2: space before break)");
 echo unitTest("test.\r\n\r\n obs@syntax.com", false, "obs-fws must have at least one WSP per line");
-echo unitTest("\"null \\\0\"@char.com", true, "can have escaped null character");
-echo unitTest("\"null \0\"@char.com", false, "cannot have unescaped null character");
-echo unitTest("null\\\0@char.com", false, "escaped null must be in quoted string");
+echo unitTest("\"null \\ \"@char.com", true, "can have escaped null character");
+echo unitTest("\"null  \"@char.com", false, "cannot have unescaped null character");
+echo unitTest("null\\ @char.com", false, "escaped null must be in quoted string");
+echo unitTest("cdburgess+!#\$%&'*-/=?+_{}|~test@gmail.com", true, "Example given in comments");
 ?>
 </body>
 
